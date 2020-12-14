ac0b70e77b870d6af2b7c3a78b5fca3dc60f196d
faystart <16603520+faystart@users.noreply.github.com>
2020-12-14 15:47:44 +0800

    change sidebar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,9 +3,9 @@ module.exports = {
     description: '关于ESG，包括ESG的标准有哪些，公司应如何披露ESG、提升ESG评级。Environmental, Social, Governance and more.', // meta 中的描述文字，用于SEO
     // 注入到当前页面的 HTML <head> 中的标签
 	base: '/' ,
-     head: [
+    head: [
    
-    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+    ['meta', { name: 'msapplication-TileColor', content: '#333333' }]
   ],  
   themeConfig: {
         logo: '/esg.png',  //网页顶端导航栏左上角的图标
@@ -13,7 +13,10 @@ module.exports = {
         nav: [           
             //格式一：直接跳转，'/'为不添加路由，跳转至首页
             { text: '首页', link: '/' }, 
-			{ text: '公司ESG披露实践', link: '/how/ESG-guide' }, 		
+			{ text: 'ESG 介绍', link: '/about/ESG' }, 
+			{ text: 'ESG 标准', link: '/criteria/ESG-Criteria' }, 
+			{ text: 'ESG 应用', link: '/apply/ESG-rating' }, 
+			{ text: 'ESG披露', link: '/how/ESG-guide' }, 		
            			
             //格式二：添加下拉菜单，link指向的文件路径
             {
@@ -37,7 +40,7 @@ module.exports = {
         
         //侧边导航栏：会根据当前的文件路径是否匹配侧边栏数据，自动显示/隐藏
         sidebar: {
-            '/':[         
+            '/about/':[         
                 {
                     title: 'ESG介绍',   // 一级菜单名称
                     collapsable: false, // false为默认展开菜单, 默认值true是折叠,
@@ -48,7 +51,8 @@ module.exports = {
 						['/about/ESG-investment-history.md', 'ESG投资史']
                     ]
                 },
-                {
+				       ],
+                '/criteria/':[ {
                     title: 'ESG标准',
                     collapsable: false, 
 					sidebarDepth: 1, 
@@ -59,7 +63,8 @@ module.exports = {
 						['/criteria/hkex-new-esg-guide-2019','港交所ESG解读']
                     ]
                 },
-				
+				             ],
+				 '/apply/':[
 				 {
                     title: 'ESG应用',
                     collapsable: false, 
@@ -72,17 +77,20 @@ module.exports = {
 						['/apply/ESG-investment-and-social-value','ESG投资与价值']
                     ]
                 },
+				          ],
+				'/how/':[
 				{
-			 title: 'ESG披露实践',   // 一级菜单名称
+			        title: 'ESG披露实践',   // 一级菜单名称
                     collapsable: false, // false为默认展开菜单, 默认值true是折叠,
                     sidebarDepth: 1,    //  设置侧边导航自动提取markdown文件标题的层级，默认1为h2层级
                     children: [
                         ['/how/ESG-guide.md', 'ESG披露指南'],  //菜单名称为'子菜单1'，跳转至/pages/folder1/test1.md
-                        ['contact', '帮助']
-                    ]
+                        ['/how/contact', '帮助']
+                             ]
 			}
+			        ]
 				
-            ]
+      
 			
             
             //...可添加多个不同的侧边栏，不同页面会根据路径显示不同的侧边栏
